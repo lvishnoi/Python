f5ab7258f2a7480de561484f505d9ee4679ad971
Lokesh Vishnoi <lokeshvishnoi1@gmail.com>
2019-01-01 00:11:09 +0530

    added validation for return type

--- codeValidator.py
+++ codeValidator.py
@@ -20,25 +20,53 @@ for line in fileRead:
             splitLen = len(line.strip().split(" "))
             if(splitLen < 4):
                 length = len(line.split(" ")[2].strip()) - 1
-                if(line.split(" ")[2][0] == "i" and re.findall("[a-z,A-Z,0-9,_]", line.split(" ")[2][length]) == []):
-                    fileWrite.write(line)
-                    fileWrite.write("Error: Interger variable is not declared properly." + "\n")
+                if(line.split(" ")[2][0] == "i"):
+                    if(re.findall("[a-z,A-Z,0-9,_]", line.split(" ")[2][length]) == []):
+                        fileWrite.write(line)
+                        fileWrite.write("Error: Interger variable is not declared properly." + "\n")
                 elif(line.split(" ")[2][0] == "s"):
                     if(re.search("[$]", line.split(" ")[2])):
                         strFlag = 1
                     else:
                         strFlag = 2
-                    if(strFlag == 2):
+                    if(strFlag == 2):                        
                         fileWrite.write(line)
                         fileWrite.write("String should contain $ symbol." + "\n")
-                #elif(line.split(" ")[2][0] == "o"):
-                #elif(line.split(" ")[2][0] == "h"):
-                #else:
-                #   pass
+                    else:
+                        if(line.split(" ")[2][length] == "$"):
+                            fileWrite.write(line)
+                            fileWrite.write("String should contain default size." + "\n")
+                elif(line.split(" ")[2][0] == "o"):
+                    #need to check object declaration
+                    if(re.findall("[a-z,A-Z,0-9,_]", line.split(" ")[2][length]) == []):
+                        fileWrite.write(line)
+                        fileWrite.write("Error: Object variable is not declared properly." + "\n")
+                elif(line.split(" ")[2][0] == "h"):
+                    if(re.findall("[a-z,A-Z,0-9,_]", line.split(" ")[2][length]) == []):
+                        fileWrite.write(line)
+                        fileWrite.write("Error: Handle variable is not declared properly." + "\n")
+                else:
+                    fileWrite.write(line)
+                    fileWrite.write("Error: Invalid variable type." + "\n")
             else:
                 fileWrite.write(line)
                 fileWrite.write("Error: Multiple declarations or initialization of variable with declaration." + "\n")
-
+    
+    if(line.split(" ")[0] == "RETURN"):
+        returnLen = len(line.strip().split(" "))
+        if(returnLen != 2):
+            fileWrite.write(line)
+            fileWrite.write("Error: Return should have a variable." + "\n")
+        while True:
+            checkVar = next(fileRead)
+            if(checkVar.split(" ")[0].strip() == "REM" or checkVar.split(" ")[0].strip() == ""):
+                continue
+            elif(checkVar.strip() == "END SUB"):
+                break
+            else:
+                fileWrite.write(line)
+                fileWrite.write("Error: No code should lie after return statement." + "\n")
+                break
 
 fileRead.close()
 fileWrite.close()
